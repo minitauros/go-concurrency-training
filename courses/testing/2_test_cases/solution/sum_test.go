e90467fc0da5b6d8b19d570f8bb0674fa4f57720
Stephan Grasmeijer <minitauros@gmail.com>
2026-05-08 09:47:14 +0200

    Remove alias

--- courses/testing/2_test_cases/solution/sum_test.go
+++ courses/testing/2_test_cases/solution/sum_test.go
@@ -3,26 +3,26 @@ package solution
 import (
 	"testing"
 
-	hygiene "github.com/minitauros/go-concurrency-training/courses/testing/2_test_cases"
+	"github.com/minitauros/go-concurrency-training/courses/testing/2_test_cases"
 )
 
-func Test_sum(t *testing.T) {
+func Test_Sum(t *testing.T) {
 	t.Run("If input is lower than 2, returns error", func(t *testing.T) {
-		err := hygiene.Sum(1)
+		err := test_cases.Sum(1)
 		if err == nil {
 			t.Error("expected error, but got nil")
 		}
 	})
 
 	t.Run("If input is equal to 2, returns nil", func(t *testing.T) {
-		err := hygiene.Sum(2)
+		err := test_cases.Sum(2)
 		if err != nil {
 			t.Error("expected nil, but got error")
 		}
 	})
 
 	t.Run("If input is higher than 2, returns nil", func(t *testing.T) {
-		err := hygiene.Sum(3)
+		err := test_cases.Sum(3)
 		if err != nil {
 			t.Error("expected nil, but got error")
 		}
