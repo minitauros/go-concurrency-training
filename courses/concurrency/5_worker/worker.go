63cfba6a908fdedb8ee690d1d05819bdc8f9eb06
Stephan Grasmeijer <minitauros@gmail.com>
2026-05-11 12:03:27 +0200

    Update comment

--- courses/concurrency/5_worker/worker.go
+++ courses/concurrency/5_worker/worker.go
@@ -1,6 +1,7 @@
 package worker
 
 func handleWork(concurrency int, workCh chan func()) {
-	// Make sure that work coming in on the work channel is handled with the given concurrency.
+	// Make sure that work coming in on the work channel is handled with exactly the given concurrency.
+	// For example, if the given concurrency is 3, execute no more than 3 pieces of work concurrently.
 	// The work channel is automatically closed (by the test) as soon as all work has been handled.
 }
